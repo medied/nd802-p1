cf21921b6b8e1169420f14b6c0c54c643c109f26
Eduardo Medina <medina.eag@gmail.com>
2016-09-20 16:50:58 -0700

    Made dates more readable (comments are for a future commit, omit)

--- client/components/events/events.js
+++ client/components/events/events.js
@@ -3,6 +3,46 @@ import { Template } from 'meteor/templating';
 
 import { Events } from '../../../imports/api/events-api.js';
 
+// Template.createEventTemp.onRendered(function () {
+// 	$("#input-event").validate({
+// 		rules: {
+// 			eventName: {
+// 				required: true
+// 			},
+// 			eventType: {
+// 				required: true
+// 			},
+// 			eventHost: {
+// 				required: true
+// 			},
+// 			eventStart: {
+// 				required: true
+// 			},
+// 			eventEnd: {
+// 				required: true
+// 			},
+// 			eventGuest: {
+// 				required: true
+// 			},
+// 			eventAddress: {
+// 				required: true
+// 			},
+// 			eventCity: {
+// 				required: true
+// 			},
+// 			eventState: {
+// 				required: true
+// 			},
+// 			eventZip: {
+// 				required: true
+// 			},
+// 			eventCountry: {
+// 				required: true
+// 			}
+// 		}
+// 		// messages: {} // (validate dates)
+// 	});
+// });
 
 Template.createEventTemp.events({
 	'submit form': function(event) {
@@ -14,9 +54,31 @@ Template.createEventTemp.events({
 		// Construct event object 
 		for (var i = 0; i < targetForm.length - 1; i++) {
 			if (targetForm[i].localName != "fieldset") {
-				var theKey = targetForm[i].name;
-				var theValue = targetForm[i].value;
-				eventObj[theKey] = theValue;
+				// Make date and time more friendly readable 
+				if (targetForm[i].name == "eventStart") {
+					console.log("Processing start date");
+					// var dateKey = targetForm[i].name;
+					var startHolder = new Date(targetForm[i].value);
+					
+					var eventStartDate = startHolder.toDateString();
+					var eventStartHour = startHolder.getHours();
+
+					eventObj["eventStartDate"] = eventStartDate;
+					eventObj["eventStartHour"] = eventStartHour;
+				}else if (targetForm[i].name == "eventEnd") {
+					console.log("Processing end date");
+					var endHolder = new Date(targetForm[i].value);
+					
+					var eventEndDate = endHolder.toDateString();
+					var eventEndHour = endHolder.getHours();
+
+					eventObj["eventEndDate"] = eventEndDate;
+					eventObj["eventEndHour"] = eventEndHour;
+				} else {
+					var theKey = targetForm[i].name;
+					var theValue = targetForm[i].value;
+					eventObj[theKey] = theValue;
+				}
 			}
 		}
 
@@ -27,3 +89,8 @@ Template.createEventTemp.events({
 		Session.set("createNewEvent", false);
 	}
 });
+
+// $.validator.addMethod( 'bookUnique', ( title ) => {
+//   let exists = Books.findOne( { "title": title }, { fields: { "title": 1 } } );
+//   return exists ? false : true;
+// });
